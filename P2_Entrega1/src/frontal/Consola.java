b2596266a6417450df4ec6d8889f2ecb1a468620
DavidCepeda13 <j.cepedab@uniandes.edu.co>
2024-05-12 08:51:41 -0500

    ConsolaAdmin

--- P2_Entrega1/src/frontal/Consola.java
+++ P2_Entrega1/src/frontal/Consola.java
@@ -1,6 +1,8 @@
 package frontal;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 import modelo.Galeria;
 
@@ -13,15 +15,34 @@ public abstract class Consola {
 		this.galeria = galeria;
 	}
 	
-	protected boolean iniciarSesion(String idUsuario, String contrasenia) {
+	protected boolean iniciarSesion(String idUsuario, String contrasenia) throws Exception {
 		if (this.galeria.getUsuario(idUsuario).login(contrasenia)) {
 			return true;
 		}
-		return false;
+		throw new Exception("El ID del usuario o la clave no son correctas");
 	}
+	
+	protected String getNombreGaleria() {
+		return this.galeria.getNombre();
+	}
+	
 	
 	public abstract void mostrarMenu();
 	
-	public abstract void ejecutarOpcion();
+	public abstract void ejecutarOpcion(String opcion);
 	
+	
+//< Input Dinamico >=============================================================================================x>
+	protected String input(String texto) {
+		try {
+			System.out.print(texto);
+			BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+			String input = reader.readLine();
+			return input;
+		} catch (IOException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+//<x=============================================================================================================x>
 }
--- P2_Entrega1/src/frontal/ConsolaAdministrador.java
+++ P2_Entrega1/src/frontal/ConsolaAdministrador.java
@@ -15,13 +15,84 @@ public class ConsolaAdministrador extends Consola{
 	
 	@Override
 	public void mostrarMenu() {
-		System.out.println("< Bienvenido >");
+		String user = input("Ingrese su ID de usuario: ");
+		String contrasenia = input("Ingrese su contrasenia: ");
+		try {
+			if (iniciarSesion(user, contrasenia)) {
+				String opcion = "opcion";
+				do {
+					System.out.println("< Bienvenido >");
+					System.out.println("\t1. Crear pieza");
+					System.out.println("\t2. Confirmar venta");
+					System.out.println("\t3. Registrar empleado");
+					System.out.println("\t4. Cambiar cupo de cliente");
+					System.out.println("\t5. Crear subasta");
+					System.out.println("\t6. Activar subasta");
+					System.out.println("\t0. Salir");
+					opcion = input("Seleccione una opción:\t");
+					ejecutarOpcion(opcion);
+				} while (!opcion.equals("0"));
+			}
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
 	}
-
-	@Override
-	public void ejecutarOpcion() {
-		// TODO Auto-generated method stub
+	
+	
+	
+	private void crearPieza() {
+		
+	}
+	
+	private void confirmarVenta() {
 		
 	}
 
+	private void registrarEmpleado() {
+		
+	}
+	
+	private void cambiarCupo() {
+		
+	}
+	
+	private void crearSubasta() {
+		
+	}
+	
+	private void activarSubasta() {
+		
+	}
+	
+	
+	
+	@Override
+	public void ejecutarOpcion(String opcion) {
+		switch (opcion) {
+		case "1":
+			crearPieza();
+			break;
+		case "2":
+			confirmarVenta();
+			break;
+		case "3":
+			registrarEmpleado();
+			break;
+		case "4":
+			cambiarCupo();
+			break;
+		case "5":
+			crearSubasta();
+			break;
+		case "6":
+			activarSubasta();
+			break;
+		case "0":
+			System.out.println("Gracias por ser parte de " + super.getNombreGaleria());
+			break;
+		default:
+			System.out.println("Opcion invalida");
+		}
+	}
 }
+
