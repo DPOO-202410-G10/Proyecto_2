1b59fa927dc1e452384989300f8224e1bdf4fce1
ctellez22 <142060772+ctellez22@users.noreply.github.com>
2024-05-12 22:56:25 -0500

    pruebas con error

--- P2_Entrega1/src/pruebas/pruebas.java
+++ P2_Entrega1/src/pruebas/pruebas.java
@@ -1,9 +1,20 @@
-import static org.junit.Assert.*;
-import org.junit.Test;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTimeout;
+
+import java.time.Duration;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import modelo.Pieza;
+
 import java.util.Arrays;
 import java.util.List;
 
-public class PiezaTest {
+public class pruebas {
 
     @Test
     public void testConstructorAndGetters() {
@@ -35,5 +46,39 @@ public class PiezaTest {
         assertEquals(anioCreacion, pieza.getAnioCreacion());
     }
 
-    
+    @Test
+    public void testSetters() {
+        Pieza pieza = new Pieza("123", "Obra de arte", "Ciudad", 10.5, 20.7, "2024-12-31", true,
+                100.0, Arrays.asList("Autor 1", "Autor 2"), "Propietario", "2020");
+
+        // Probamos los setters
+        pieza.setIdPieza("456");
+        assertEquals("456", pieza.getIdPieza());
+
+        pieza.setTitulo("Nueva obra");
+        assertEquals("Nueva obra", pieza.getTitulo());
+
+        pieza.setLugarCreacion("Nuevo lugar");
+        assertEquals("Nuevo lugar", pieza.getLugarCreacion());
+
+        pieza.setFechaLimite("2025-12-31");
+        assertEquals("2025-12-31", pieza.getFechaLimite());
+
+        pieza.setValorFijo(false);
+        assertFalse(pieza.isValorFijo());
+
+        pieza.setValorinicial(200.0);
+        assertEquals(200.0, pieza.getValorInicial(), 0.001);
+
+        pieza.setAutores(Arrays.asList("Nuevo Autor"));
+        assertEquals(Arrays.asList("Nuevo Autor"), pieza.getAutores());
+
+        pieza.setPropietario("Nuevo Propietario");
+        assertEquals("Nuevo Propietario", pieza.getPropietario());
+
+        pieza.setAnioCreacion("2021");
+        assertEquals("2021", pieza.getAnioCreacion());
+    }
+
+
 }
